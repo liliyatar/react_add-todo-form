d7172bb5b4de58b0a6223db2382c81b5c559e991
Liliya Taran <liliyataran2017@gmail.com>
2019-12-11 08:14:53 +0200

    Updates

---
+++ precache-manifest.d2b0c5b0bfc8819e8bb963f6ef1007bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3717ec53f2e34758fa7db97e07ade6b8",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "c78630353cb666200758",
+    "url": "/react_add-todo-form/static/css/main.773e3455.chunk.css"
+  },
+  {
+    "revision": "4d1c0469dc641d2f7269",
+    "url": "/react_add-todo-form/static/js/2.efce9acf.chunk.js"
+  },
+  {
+    "revision": "c78630353cb666200758",
+    "url": "/react_add-todo-form/static/js/main.753fad1b.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
